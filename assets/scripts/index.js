b778101ff8751717c5e1947d3796e5d2c5bf2eb0
eabrigham <eabrigham@gmail.com>
2018-08-09 10:21:09 -0400

    Basic authentication functioning.
    Dynamic modals not yet impemented.

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -1,6 +1,8 @@
 "use strict";
 const blogApi = require("./blog/blogs-Api.js");
 
+const authEvents = require('./auth/events.js')
+
 // use require with a reference to bundle the file and use it in this file
 // const example = require('./example')
 
